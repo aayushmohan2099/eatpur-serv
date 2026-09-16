d3cdc22610d7133e32afd3494a2c86ce948ed21f
shailendra singh kushwaha <skushawah455@gmail.com>
2026-09-16 16:44:21 +0530

    make get api to show review to admin dashboard

--- messaging/urls.py
+++ messaging/urls.py
@@ -3,8 +3,10 @@ messaging/urls.py
 """
 
 from django.urls import path
-from .views import *
+from .views import google_form_webhook, get_all_reviews
 
 urlpatterns = [
     path("google-form-response/", google_form_webhook),
+
+    path("all-reviews/", get_all_reviews),
 ]
